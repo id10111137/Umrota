ea250c12a6208a4807dd0d4f3808cf44ffcf0654
Tatang Roswandi Ganda Wijaya <id.10111137@gmail.com>
2019-06-15 21:29:27 +0700

    Initial Commit

---
+++ Umrota/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Umrota
+//
+//  Created by Tatang Roswandi Ganda Wijaya on 15/06/19.
+//  Copyright © 2019 Tatang Roswandi Ganda Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
